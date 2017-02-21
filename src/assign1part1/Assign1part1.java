7e8fdeeb7fe4d96b07e1fa9017a96a0cad1a706c
AlexandreMelano <alexandremelano@yahoo.com>
2017-02-21 17:16:17 -0500

    Person class update

---
+++ src/assign1part1/Assign1part1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign1part1;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author User1
+ */
+public class Assign1part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person Person1 = new Person ("Alex", "Melano","44 Geddes", "Barrie", "Ontario", "L4NB2",LocalDate.of(1995, Month.FEBRUARY, 16));
+        System.out.printf("Person1 is %s %s%n", Person1.getFirstName(),Person1.getLastName());
+        System.out.printf("the employee was born on %s, so %s is %d%n",Person1.getBirthdate(), Person1.getFirstName(),Person1.getAge(LocalDate.MIN));
+        System.out.printf("%s's address is %s", Person1.getFirstName(),Person1.toString());
+        
+    }
+    
+}
